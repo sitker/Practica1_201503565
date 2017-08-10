84a4c5d8f4cf4c862dab68352f8675812444bd3c
Merari <Merari@DESKTOP-Q8BBL7C>
2017-08-10 17:23:28 -0600

    buscaminas_201503565 (1.0)

---
+++ src/buscaminas_201503565/BuscaMinas_201503565.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas_201503565;
+
+/**
+ *
+ * @author Merari
+ */
+public class BuscaMinas_201503565 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
